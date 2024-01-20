e45c247b71e33130edcf7099a3d42db207c054ed
Yashar Najafi <yasharnajafi6946@gmail.com>
2024-01-20 03:12:22 +0300

    start practice docker

---
+++ app/api/categories/route.ts
@@ -0,0 +1,10 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export const GET = () => {
+    return new NextResponse("Hello", {status: 200});
+}
+
+
+export const POST = () => {
+    return new NextResponse("Hello", {status: 200});
+}
